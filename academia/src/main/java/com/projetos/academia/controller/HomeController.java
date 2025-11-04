45d87f82790022b29657d5082493dd5ecc9ed6d9
Nicole Lourenço <nicolelourenco38@gmail.com>
2025-11-04 08:08:57 -0300

    back end do site

---
+++ academia/src/main/java/com/projetos/academia/controller/HomeController.java
@@ -0,0 +1,32 @@
+package com.projetos.academia.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        return "home";
+    }
+
+    @GetMapping("/blog")
+    public String blog(Model model) {
+        return "blog";
+    }
+
+    @GetMapping("/contato")
+    public String contato(Model model) {
+        return "contato";
+    }
+
+    @GetMapping("/aulas")
+    public String aulas(Model model) {
+        return "aulas";
+    }
+
+
+
+}
